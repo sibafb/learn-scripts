5d7fcbbc3d6baf5a5fd90e7060f9095febd2997f
AkimotoHaruma <siba16fb@gmail.com>
2023-02-26 03:15:21 +0900

    add_get_display_size

---
+++ tips/get_display_size.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
+Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+
+echo "Xaxis" $Xaxis
+echo "Yaxis" $Yaxis
+
