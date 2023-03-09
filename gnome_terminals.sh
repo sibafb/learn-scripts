a5352c5a91aa2be62c0f15ea682c9d29e3bd6ca8
AkimotoHaruma <siba16fb@gmail.com>
2023-03-10 07:18:01 +0900

    Add gnome terminal launch options (title A~F)

--- gnome_terminals.sh
+++ gnome_terminals.sh
@@ -3,9 +3,16 @@
 Xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
 Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
 
+export_ip () {
+    echo "hello"
+    echo "world"
+}
 echo "Xaxis" $Xaxis
 echo "Yaxis" $Yaxis
 
-gnome-terminal --geometry=80x20+0+0
-gnome-terminal --geometry=80x20+0+500 -- bash -c "echo 'Hello Bash!'; top" 
-
+gnome-terminal --geometry=80x7+0+0      -- bash -c "echo 'Hello Bash!'; top"  --title A
+gnome-terminal --geometry=80x7+0+300    -- bash -c "echo 'Hello Bash!'; top"  --title B
+gnome-terminal --geometry=80x7+0+600    -- bash -c "echo 'Hello Bash!'; top"  --title C
+gnome-terminal --geometry=80x7+1500+0   -- bash -c "echo 'Hello Bash!'; top"  --title D
+gnome-terminal --geometry=80x7+1500+300 -- bash -c "echo 'Hello Bash!'; top"  --title E
+gnome-terminal --geometry=80x7+1500+600 -- bash -c "echo 'Hello Bash!'; top"  --title F
