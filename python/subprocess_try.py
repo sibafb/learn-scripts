87025b1fc51a0b0efe3bacaf6570f02e7c612771
akimotoharuma <siba16fb@gmail.com>
2023-03-18 11:38:20 +0900

    Add setting Ivent Handler termination.

--- python/subprocess_try.py
+++ python/subprocess_try.py
@@ -1,15 +1,16 @@
 import os
 import subprocess
 import time
+import signal 
 
 if __name__ == '__main__':
 
-    print(str(os.getpid()) + ": start hello")
+    print("PID: " + str(os.getpid()) + ": start hello")
     p = subprocess.Popen('exec python whilehello.py', shell=True)
     time.sleep(5)
     p.kill()
     p.wait()
-    print(str(os.getpid()) + ": kill process") 
+    print("PID: " + str(os.getpid()) + ": kill process") 
     # $ python subprocess_try.py 
     # 2926273: start hello
     # 2926274: hello
@@ -18,3 +19,21 @@ if __name__ == '__main__':
     # 2926274: hello
     # 2926274: hello
     # 2926273: kill process
+
+    # signal 非同期イベントにハンドラを設定する
+    # https://docs.python.org/ja/3/library/signal.html
+    print("PID: " + str(os.getpid()) + ": start hello")
+    pr = subprocess.Popen('exec python whilehello.py', shell=True)
+    time.sleep(5)
+
+    pr.send_signal(signal.SIGINT)
+    pr.wait()
+    print("PID: " + str(os.getpid()) + ": kill process") 
+    # 3424410: start hello
+    # 3424434: hello
+    # 3424434: hello
+    # 3424434: hello
+    # 3424434: hello
+    # 3424434: hello
+    # good bye.
+    # 3424410: kill process
--- python/whilehello.py
+++ python/whilehello.py
@@ -3,7 +3,11 @@ import time
 import subprocess
 
 if __name__ == '__main__':
-    while True:
-        print(str(os.getpid()) + ": hello")
-        time.sleep(1)
-    print(str(os.getpid()) + "kill hello")
+    try:
+        while True:
+            print("PID: " + str(os.getpid()) + ": hello")
+            time.sleep(1)
+        print("PID: " + str(os.getpid()) + "kill hello")
+    except KeyboardInterrupt:
+        print("PID: " + str(os.getpid()) + "good bye.")
+        pass
