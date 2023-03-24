e287eb4bda674d1403011e1e29e192eaf34af753
akimotoharuma <siba16fb@gmail.com>
2023-03-25 05:12:51 +0900

    Add output example.

--- python/csv_sandbox.py
+++ python/csv_sandbox.py
@@ -6,3 +6,6 @@ if __name__=='__main__':
         spamreader = csv.reader(csvfile, delimiter=',')
         for row in spamreader:
             print(', '.join(row))
+            # 1, 2, 3
+            # 4, 5, 6
+            # 7, 8, 9
